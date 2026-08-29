06ef722f5b739ea6ff7f0fb3ee440b5ea0d79de4
SergiyKoAI <sergeyapik@gmail.com>
2026-08-29 17:21:36 +0300

    Add files via upload

---
+++ content/links.js
@@ -0,0 +1,18 @@
+// EDIT THIS FILE to change external links.
+
+const VIDEO_LINKS = {
+  archie: "https://www.instagram.com/p/DcgkEPrKwvA/",
+  ember: "https://www.instagram.com/p/DU3dgbvgmAR/",
+  shtrudel: "https://www.instagram.com/p/DWJ0G7CAi4r/",
+  music: "https://www.instagram.com/p/DZSirk6MmNv/"
+};
+
+const CONTACT_LINKS = {
+  instagram: "https://www.instagram.com/sergiyko_ai_studio/",
+  telegram: "https://t.me/SergiyKo_AI",
+  email: "mailto:sergeyapik@gmail.com"
+};
+
+const CERTIFICATE_LINKS = {
+  google: "https://www.coursera.org/account/accomplishments/specialization/EPKTD5TBQQJD"
+};
